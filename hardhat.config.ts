7d7a8e4b60311d966d7b67b9d5b1d3f4c9ec6bc7
Ming Jiang <minkyn@users.noreply.github.com>
2023-03-02 17:19:11 -0700

    Update deploy script and network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`0x${process.env.ACCOUNT_PRIVATE_KEY}`]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
   solidity: "0.8.17",
 };
 
